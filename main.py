39a22b1fd0f90ceaf6591a2a9e695b54f96fbf26
Dogheart666 <rocketgo61@gmail.com>
2025-04-10 01:38:39 +0200

    удалил

--- main.py
+++ main.py
@@ -1,18 +1,1 @@
-from flask import Flask, request, jsonify
-import requests
 
-app = Flask(__name__)
-
-@app.route("/", methods=["POST"])
-def receive_data():
-    data = request.json
-    if not data:
-        return jsonify({"error": "No JSON data received"}), 400
-
-    sheet_url = "https://script.google.com/macros/s/AKfycbwQiuDcdFWMix9Qs4ol4twfUoGviI7gF3YwHHozv0wqSbZT71VrsSz9T-Af6ijwM6yXTw/exec"
-    response = requests.post(sheet_url, json=data)
-
-    return jsonify({"status": "ok", "gs_response": response.text}), response.status_code
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
