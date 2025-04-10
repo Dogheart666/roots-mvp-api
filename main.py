f054ad31e54837248154a8b6bcc9c74ed388c72a
Dogheart666 <rocketgo61@gmail.com>
2025-04-10 23:54:51 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import requests
+import os
 
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/", methods=["POST"])
+def receive_data():
+    data = request.json
+    if not data:
+        return jsonify({"error": "No JSON data received"}), 400
+
+    sheet_url = "https://script.google.com/macros/s/AKfycbwQiuDcdFWMix9Qs4ol4twfUoGviI7gF3YwHHozv0wqSbZT71VrsSz9T-Af6ijwM6yXTw/exec"
+    response = requests.post(sheet_url, json=data)
+
+    return jsonify({"status": "ok", "gs_response": response.text}), response.status_code
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))  # берем порт из окружения
+    app.run(host="0.0.0.0", port=port)
